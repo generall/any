7c5abe91b310624636fde6cfec26bea43a99e65d
generall <generall@generall-P5K-E>
2012-11-18 01:44:48 +0400

    Template Magic

--- any/src/any.cpp
+++ any/src/any.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : any.cpp
-// Author      : 
+// Author      :
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
@@ -9,7 +9,72 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+class A
+{
+public:
+	virtual void print()=0;
+	virtual ~A()
+	{
+	}
+	;
+};
+
+template<class T>
+class B: public A
+{
+public:
+	T t;
+	void print()
+	{
+		cout << t << std::endl;
+	}
+	B(T _t)
+	{
+		t = _t;
+	}
+private:
+	B()
+	{
+	}
+};
+
+class any
+{
+public:
+	A* ref;
+	template<class T>
+	any(T t)
+	{
+		ref = new B<T>(t);
+	}
+	void print()
+	{
+		ref->print();
+	}
+	template<class T>
+	T operator=(T t)
+	{
+		if (ref != NULL)
+			delete ref;
+		ref = new B<T>(t);
+		return t;
+	}
+	~any()
+	{
+		delete ref;
+	}
+	any()
+	{
+		ref = NULL;
+	}
+};
+
+int main()
+{
+	any t=123;
+	t.print();
+	any s="str";
+	s.print();
+	//cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
